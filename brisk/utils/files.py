797aaf052be21cb6f85149487dedb73435b58407
SmRanaldi <sm.ranaldi@gmail.com>
2022-03-16 16:30:28 +0100

    Get names of IMU files from folder

--- brisk/utils/files.py
+++ brisk/utils/files.py
@@ -1,8 +1,10 @@
 from tkinter import Tk, filedialog
 
+import os
+
 # Asks for a folder where the data is stored, 
 # as it's saved by the system
-def load_folder():
+def get_folder():
     root=Tk()
     root.withdraw()
 
@@ -11,3 +13,10 @@ def load_folder():
     root.destroy()
 
     return open_file
+
+# Gets IMU data filenames from a folder
+def get_imu_filename(dirname):
+
+    fnames = os.listdir(dirname)
+
+    return [x for x in fnames if x.startswith('data_')]
