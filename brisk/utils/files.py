0c2043a6fab21c8e12ab6d7b3339b7c04ec8273b
SmRanaldi <sm.ranaldi@gmail.com>
2022-03-16 17:12:58 +0100

    Cross-platform path names

--- brisk/utils/files.py
+++ brisk/utils/files.py
@@ -7,8 +7,9 @@ import os
 def get_folder():
     root=Tk()
     root.withdraw()
+    root.focus_set()
 
-    open_file = filedialog.askdirectory()
+    open_file = filedialog.askdirectory(parent=root)
 
     root.destroy()
 
@@ -20,3 +21,9 @@ def get_imu_filename(dirname):
     fnames = os.listdir(dirname)
 
     return [x for x in fnames if x.startswith('data_')]
+
+# Join path names with a forward slash
+# for cross-platform support
+def join_path(components):
+
+    return os.path.normpath(os.path.join(*components))
