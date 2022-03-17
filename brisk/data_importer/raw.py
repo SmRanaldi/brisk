2b0ba92a9474e2300dd0c0f2eb1a3283ad831917
SmRanaldi <sm.ranaldi@gmail.com>
2022-03-17 10:47:54 +0100

    Some configuration

--- brisk/data_importer/raw.py
+++ brisk/data_importer/raw.py
@@ -8,3 +8,12 @@ from brisk.utils import path
 def import_imu_data(filename):
 
     return pd.read_csv(filename)
+
+# Import data and save to the directory
+# in the config files
+def save_imu_data():
+
+    base_dir = path.get_folder()
+    files = path.get_imu_filename(base_dir)
+
+    return
