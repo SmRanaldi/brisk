2e300b381cc64a73d9b5852846f8fb2f9a768302
SmRanaldi <sm.ranaldi@gmail.com>
2022-04-20 12:31:43 +0200

    Calculated parameters.
    Need to check the autocorrelation, in its trimmed version. Moreover, need to find a way of optimizing that

--- brisk/utils/signal.py
+++ brisk/utils/signal.py
@@ -16,6 +16,22 @@ def norm_autocorrelation(a):
 
     return np.asarray(corr_temp)
 
+# Custom trimmed autocorrelation
+def trim_norm_autocorrelation(a, n_samples):
+
+    half_n = int(np.round(n_samples/2))
+
+    a = a.squeeze()
+
+    corr_temp = [np.corrcoef(a[:-t], a[t:])[0,1] 
+        for t in range(
+                1 + int(np.round(a.shape[0]/2)) - half_n, 
+                1 + int(np.round(a.shape[0]/2)) + half_n, 
+            )
+        ]
+
+    return np.asarray(corr_temp)
+
 # Custom template matching
 def norm_templatematching(a,template):
 
